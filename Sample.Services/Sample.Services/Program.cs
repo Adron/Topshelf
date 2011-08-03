a18b473cd0fd5e3050512c04919c369ad8ae0789
Adron Hall <adronhall@gmail.com>
2011-08-03 15:50:30 -0700

    First basic Topshelf Sample completed.

---
+++ Sample.Services/Sample.Services/Program.cs
@@ -0,0 +1,28 @@
+﻿using Topshelf;
+
+namespace Sample.Services
+{
+    class Program
+    {
+        static void Main()
+        {
+            HostFactory.Run(hostConfigurator =>
+            {
+                hostConfigurator.Service<StatusCheckIn>(
+                    serviceConfigurator =>
+                {
+                    serviceConfigurator.SetServiceName("TS");
+                    serviceConfigurator.ConstructUsing(
+                        name => new StatusCheckIn());
+                    serviceConfigurator.WhenStarted(tc => tc.Start());
+                    serviceConfigurator.WhenStopped(tc => tc.Stop());
+                });
+
+                hostConfigurator.RunAsLocalSystem();
+                hostConfigurator.SetDescription("Sample  Host");
+                hostConfigurator.SetDisplayName("Display Name");
+                hostConfigurator.SetServiceName("Topshelf Service");
+            });
+        }
+    }
+}
---
+++ Sample.Services/Sample.Services/StatusCheckIn.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Timers;
+
+namespace Sample.Services
+{
+    public class StatusCheckIn
+    {
+        readonly Timer _timer;
+
+        public StatusCheckIn()
+        {
+            _timer = new Timer(5000) { AutoReset = true };
+            _timer.Elapsed += TimeHappened;
+        }
+
+        static void TimeHappened(object sender, EventArgs eventArgs)
+        {
+            Console.WriteLine("Status Time: {0} checking in.", 
+                DateTime.Now);
+            Console.WriteLine("Sender Type: {0}",
+                sender.GetType());
+            Console.WriteLine("Event Argument Type: {0}",
+                eventArgs);
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
